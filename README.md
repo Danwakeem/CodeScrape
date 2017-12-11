3263b69bba1425b9149653ea4bd0c4dfd3f88388
Danwakeem <Danwakeem@users.noreply.github.com>
2017-12-11 10:09:29 -0600

    Added Travis CI Badge
    Basic tests
    
    Added code coverage
    
    Added coverage badge
    
    Added mocha reporter
    
    Adjust coverage reporter
    
    Added cors headers

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # CodeScrape
 
+[![Build Status](https://travis-ci.org/Danwakeem/CodeScrape.svg?branch=master)](https://travis-ci.org/Danwakeem/CodeScrape)
+[![Coverage Status](https://coveralls.io/repos/github/Danwakeem/CodeScrape/badge.svg?branch=master)](https://coveralls.io/github/Danwakeem/CodeScrape?branch=master)
+
 This is an AWS lambda funtion that will compile code from [CodePad](http://codepad.org) via an nifty little API.
 
 I am going to expect some kind of request body with the code and prefered language to run. The language must be compatable with [CodePad](http://codepad.org).
--- scrape.js
+++ scrape.js
@@ -5,20 +5,46 @@ const run = 'True';
 
 const parseOutput = (outputPage) => {
 	const $ = cheerio.load(outputPage);
-	const output = $($('a[name="output"] + .code .highlight pre')[1]).text();
-	return { output };
+	const codeSection = $('a[name="output"] + .code .highlight pre');
+	if (codeSection.length > 0) {
+		const output = $(codeSection[1]).text();
+		return { output };
+	} else {
+		return { output: 'Error: We are unable to run your code. Please try again later.' }
+	}
 }
 
-const compileCode = (lang, code) => {
-	return rp.post(url, { form: { lang, code, run } })
+const getRedirect = (body) => {
+	return rp.post(url, body)
 	.then(data => {})
 	.catch(redirect => {
-		return rp.get(redirect.response.headers.location)
-		.then(parseOutput);
+		if (redirect.response.statusCode === 302) {
+			return redirect.response.headers.location;
+		} else {
+			return 'Error';
+		}
+	});
+};
+
+const getCompiledHTML = (location) => {
+	return rp.get(location)
+	.then(html => html);
+};
+
+const compileCode = (lang, code) => {
+	return getRedirect({ form: { lang, code, run } })
+	.then(location => {
+		return getCompiledHTML(location)
+		.then(html => parseOutput(html));
+	}).catch(err => {
+		return { output: 'Error: Something went wrong when compiling your code. Please try again later.' };
 	});
 };
 
 
 module.exports = {
-   compileCode
+   compileCode,
+   parseOutput,
+   getCompiledHTML,
+   getRedirect
 };
